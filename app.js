5895272b9b89ad40295a1afb0feb12c728adc58c
Shahid-gds <shahid.gdsdev@gmail.com>
2023-12-28 04:54:49 +0500

    Email Styling

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.post('/submit-form', async (req, res) => {
   try {
-    const { name, company, email, size, message } = req.body;
+    const { name, company, email, phone, message } = req.body;
 
     // Create a nodemailer transporter with your email service credentials
     const transporter = nodemailer.createTransport({
@@ -30,9 +30,9 @@ app.post('/submit-form', async (req, res) => {
       subject: 'Hire Us || Contact Form',
       html: `
         <p>Name: ${name}</p>
-        <p>Company: ${company}</p>
         <p>Email: ${email}</p>
-        <p>Company Size: ${size}</p>
+        <p>Company Size: ${phone}</p>
+        <p>Company: ${company}</p>
         <p>Message: ${message}</p>
       `,
     };
@@ -41,6 +41,93 @@ app.post('/submit-form', async (req, res) => {
     const info = await transporter.sendMail(mailOptions);
 
     console.log('Email sent:', info);
+    
+   // Email content for the thank you message to the user
+const thankYouMailOptions = {
+    from: 'email@americanwebcraft.com',
+    to: email, // Use the user's email address
+    subject: 'Thank You for Contacting Us',
+    html: `
+      <html>
+        <head>
+          <style>
+            body {
+              font-family: 'Arial', sans-serif;
+              background-color: #f4f4f4;
+              color: #333;
+              margin: 0;
+              padding: 0;
+            }
+            
+            .card {
+              border: 2px solid #601417;
+              max-width: 400px;
+              background-color: #fff;
+              border-radius: 8px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+            .header {
+                background: #2d2e30;
+                text-align: center;
+                color: #fff;
+                padding: 15px;
+                font-size: 20px;
+              }
+            .dear {
+              color: #2d2e30;
+              padding-right: 20px;
+              padding-left: 20px;
+              margin-top: 20px;
+              font-size: 22px;
+            }
+            .name {
+              color: #2d2e30;
+              padding-right: 20px;
+              padding-left: 20px;
+              font-size: 20px;
+            }
+  
+            h2 {
+              color: #2d2e30;
+              padding-right: 20px;
+              padding-left: 20px;
+            }
+  
+            p {
+              line-height: 1.6;
+              padding-right: 20px;
+              padding-left: 20px;
+              font-size: 17px;
+            }
+  
+            .footer {
+              background: #2d2e30;
+              padding: 10px;
+              margin-top: 20px;
+              text-align: center;
+              color: #fff;
+              font-size: 16px;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="card">
+          <div class="header">Welcome to American Webcraft</div>
+            <div class="dear">Dear,</div>
+            <div class="name">${name}</div>
+            <h2>Thank You for Contacting Us.</h2> <br> <p>One of our representative will call you shortly on your number or your email.</p>
+            <div class="footer">
+              <p>Best regards,<br>American Webcraft</p>
+            </div>
+          </div>
+        </body>
+      </html>
+    `,
+  };
+  
+  // Send thank you email to the user
+  await transporter.sendMail(thankYouMailOptions);
+  
 
     res.status(200).send('Form submitted successfully.');
   } catch (error) {
